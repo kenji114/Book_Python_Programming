79a64206389de4dd11eec1b8599bf499bcf1a9ce
kenji <kenji114@gmail.com>
2021-04-03 18:46:08 +0900

    pca_sklearn first

--- pca_sklearn.py
+++ pca_sklearn.py
@@ -1,4 +1,14 @@
 from matplotlib.colors import ListedColormap
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('r', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min() - 1, X[: 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[: 1].max() + 1
+
+    #グリッドポイントの生成
 
 
