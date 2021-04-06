4801491d41301af36f993488661eea3714647715
kenji114 <kenji114@gmail.com>
2021-04-07 08:12:58 +0900

    pca_sklearn バージョン

--- pca_sklearn.py
+++ pca_sklearn.py
@@ -1,14 +1,94 @@
 from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
-    markers = ('r', 'x', 'o', '^', 'v')
+    markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
+    x1_min = X[:,0].min() - 1
+    x1_max = X[:,0].max() + 1
+    x2_min = X[:,1].min() - 1
+    x2_max = X[:,1].max() + 1
+
     # 決定領域のプロット
-    x1_min, x1_max = X[:, 0].min() - 1, X[: 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[: 1].max() + 1
+    # x1_min, x1_max = X[:, 0].min() - 1, X[: 0].max() + 1
+    # x2_min, x2_max = X[:, 1].min() - 1, X[: 1].max() + 1
 
-    #グリッドポイントの生成
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    # 各特徴量を１次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+
+    # 予測結果を元のグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+
+    # 軸の範囲の設定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとにサンプルをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
 
 
+def main():
+    df_wine = pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+        header=None)
+
+    # print(df_wine)
+    X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    sc = StandardScaler()
+    min_max = sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    # 主成分数を指定して、PCAのインスタンスを生成
+    pca = PCA(n_components=2)
+    # ロジスティック回帰のインスタンスを生成
+    lr = LogisticRegression()
+    # トレーニングデータやテストデータをPCAに適合させる。
+    X_train_pca = pca.fit_transform(X_train_std)
+    X_test_pca = pca.transform(X_test_std)
+
+    # トレーニグデータをロジスティック回帰に適合させる
+    lr.fit(X_train_pca, y_train)
+
+    # print(X_train_pca[:,0].min())
+    # print(X_train_pca[:,0].max())
+    # print(X_train_pca[:,1].min())
+    # print(X_train_pca[:,1].max())
+
+    # 決定境界をプロット
+    # plot_decision_regions(X_train_pca, y_train, classifier=lr)
+    # plt.xlabel('PC1')
+    # plt.ylabel('PC2')
+    # plt.legend(loc='lower left')
+    # plt.show()
+
+    # テストデータでも試してみる。
+    plot_decision_regions(X_test_pca, y_test, classifier=lr)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
